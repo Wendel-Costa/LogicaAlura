9e08ea2bf573605f121bc68f5cafff88c7d82c09
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-01-23 19:03:56 -0300

    aulas finais

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numsec = 8;
+let quant = 100
+let numsec = parseInt(Math.random()*quant + 1);
 let chute;
 let tentativas = 1;
 
 while(numsec != chute){
-    chute = prompt('Diga um número de 1 a 10');
+    chute = prompt(`Diga um número de 1 a ${quant}`);
     if (numsec == chute) {
-        alert(`Acertou! O número secreto é: ${numsec}. Você precisou de ${tentativas} tentativa(s).`);
+        break;
     } else {
         if (numsec > chute){
             alert(`Errou o número secreto é maior que ${chute}`);
@@ -17,3 +18,15 @@ while(numsec != chute){
     }
     tentativas = tentativas + 1
 }
+let num = tentativas==1 ? 'tentativa':'tentativas';
+alert(`Acertou! O número secreto é: ${numsec}. Você precisou de ${tentativas} ${num}.`);
+
+
+//Versão longa
+/*if (tentativas==1){
+    alert(`Acertou! O número secreto é: ${numsec}. Você precisou de ${tentativas} tentativa.`);
+} else{
+    alert(`Acertou! O número secreto é: ${numsec}. Você precisou de ${tentativas} tentativas.`);
+}
+*/
+
